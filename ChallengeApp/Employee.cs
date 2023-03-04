f97a5302428c91543c8db9b9993a4b257452989a
TomaszKluska <tomaszkluska@aol.co.uk>
2023-03-04 18:06:24 +0000

    Zadanie dzien6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ChallengeApp
+    {
+        public class Employee
+        {
+            private List<int> score = new List<int>();
+
+            public Employee(string name, string surname, int age)
+            {
+                this.Name = name;
+                this.Surname = surname;
+                this.Age = age;
+            }
+
+
+
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int Age { get; set; }
+
+            public int Result
+            {
+                get
+                {
+                    return this.score.Sum();
+
+                }
+
+            }
+            public void AddScore(int score)
+            {
+                this.score.Add(score);
+            }
+
+
+        }
+
+    }
+
+
+
+
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,77 +1,54 @@
-﻿using System;
-using System.ComponentModel.Design;
+﻿using ChallengeApp;
 
-int number = 456699;
-
-List<string> list = new List<string>();
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
-
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Jan","Kowalski ", 25);
+Employee employee2 = new Employee("Ewa", "Michalak", 30);
+Employee employee3 = new Employee("Adam", "Kaminski", 21);
 
 
-foreach (char letter in letters)
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(8);
+employee1.AddScore(5); 
+employee1.AddScore(8);
+
+employee2.AddScore(6);
+employee2.AddScore(7);
+employee2.AddScore(3);
+employee2.AddScore(4);
+employee2.AddScore(2);
+
+employee3.AddScore(5); 
+employee3.AddScore(8);
+employee3.AddScore(6);
+employee3.AddScore(5);
+employee3.AddScore(2);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0') 
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    employee1,  employee2,   employee3
+};
 
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+
+foreach (var Employee in employees) 
+{
+    if (Employee.Result > maxResult)
+    {
+        employeeWithMaxResult = Employee;
+        maxResult = Employee.Result;
+    }
 }
+Console.WriteLine("Pracownik z najwieksza iloscia punktow to:" + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname +  " " 
++ maxResult + "punktow" );
+Console.WriteLine("Wiek" + employeeWithMaxResult.Age);
 
-Console.WriteLine("wynik dla liczby:" + " "+ (number));
-Console.WriteLine("0=>" + counter0);
-Console.WriteLine("1=>" + counter1);
-Console.WriteLine("2=>" + counter2);
-Console.WriteLine("3=>" + counter3);
-Console.WriteLine("4=>" + counter4);
-Console.WriteLine("5=>" + counter5);
-Console.WriteLine("6=>" + counter6);
-Console.WriteLine("7=>" + counter7);
-Console.WriteLine("8=>" + counter8);
-Console.WriteLine("9=>" + counter9);
+
+
+
+
+
+
+  
+
