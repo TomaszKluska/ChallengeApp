2ea940330adddadc1f664e7163e3d514ea317d13
TomaszKluska <tomaszkluska@aol.co.uk>
2023-03-10 12:16:17 +0000

    Zadanie Dzien8

---
+++ ChallengeApp.Tests/TypeTest.cs
@@ -0,0 +1,38 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTest
+    {
+        [Test]
+        public void GetIntValues_ShouldReturnEquals()
+        {
+            //arrange
+            int number1 = 1;
+            int number2 = 1;
+
+            //assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void GetStringValues_ShouldReturnEquals()
+        {
+            //arrange
+            string employee1 = ("Adamus");
+            string employee3 = ("Adamus");
+
+            //assert
+            Assert.AreEqual(employee1, employee3);
+        }
+
+        [Test]
+        public void GetFloatValues_ShouldReturnEquals()
+        {
+            //arrange
+            float float1 = 8.5f;
+            float float2 = 8.5f;
+
+            //assert
+            Assert.AreEqual(float1, float2);
+        }
+    }
+}
