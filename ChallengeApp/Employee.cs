da9acf3d6ed5d63b75b1a21fce51f9af06976cdd
TomaszKluska <tomaszkluska@aol.co.uk>
2023-03-17 19:34:07 +0000

    Zadanie -Dzien10

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -1,59 +1,102 @@
 ﻿
 
-namespace ChallengeApp
+namespace ChallengeApp;
+
+public class Employee
+{
+    private List<float> score = new List<float>();
+
+    public Employee(string name, string surname, int age)
     {
-        public class Employee
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = age;
+    }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public int Result
+    {
+        get
         {
-            private List<int> score = new List<int>();
-
-            public Employee(string name, string surname, int age)
-            {
-                this.Name = name;
-                this.Surname = surname;
-                this.Age = age;
-            }
-
-
-
-            public string Name { get; set; }
-            public string Surname { get; set; }
-            public int Age { get; set; }
-
-            public int Result
-            {
-                get
-                {
-                    return this.score.Sum();
-
-                }
-
-            }
-            public void AddScore(int score)
-            {
-                this.score.Add(score);
-            }
-        public Statistics GetStatistics()
-        {
-            var statistics  = new Statistics();
-            statistics.Average = 0;
-            statistics.Max = float.MaxValue;
-            statistics.Min = float.MinValue;
-            
-            foreach (var score in this.score)
-            {
-                statistics.Max = Math.Max(statistics.Max, score);
-                statistics.Min = Math.Min(statistics.Min, score);
-                statistics.Average += score;
-            }
-            statistics.Average /= this.score.Count;
-            return statistics;
-
-        }
-
+            return (int)this.score.Sum();
 
         }
 
     }
+    public void AddScore(int score)
+    {
+        this.score.Add(score);
+    }
+    public Statistics GetStatistics()
+    {
+        var statistics = new Statistics();
+        statistics.Average = 0;
+        statistics.Max = float.MinValue;
+        statistics.Min = float.MaxValue;
+
+        foreach (var score in this.score)
+        {
+            statistics.Max = Math.Max(statistics.Max, score);
+            statistics.Min = Math.Min(statistics.Min, score);
+            statistics.Average += score;
+        }
+        statistics.Average /= this.score.Count;
+        return statistics;
+
+    }
+
+    public void AddScore(float score) 
+        
+    {
+        if(score >= 0 && score<=100)
+        {
+            
+            this.score.Add(score);
+        }
+        else
+        {
+            Console.WriteLine("Invalid score value");
+        }
+    }
+
+    public void AddScore(string score)
+    {
+        if (float.TryParse(score, out float result))
+        {
+            this.score.Add(result);
+        }
+        else
+        {
+            Console.WriteLine("String is not a float");
+        }
+    }
+
+    public void AddScore(double score)
+    {
+            var DoubleAsFloat = (float)score;
+            this.AddScore(DoubleAsFloat);
+           
+    }
+
+    public void AddScore(decimal score)
+    {
+        var DoubleAsFloat = (float)score;
+        this.AddScore(Result);
+    }
+   
+    
+    public void AddScore(long score)
+    {
+        var DoubleAsFloat = (float)score;
+        this.AddScore(Result);
+    }
+ }
+
+
+
 
 
 
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -5,55 +5,54 @@ Employee employee2 = new Employee("Ewa", "Michalak", 30);
 Employee employee3 = new Employee("Adam", "Kaminski", 21);
 
 
-employee1.AddScore(9);
-employee1.AddScore(7);
-employee1.AddScore(8);
-employee1.AddScore(5); 
-employee1.AddScore(8);
+employee1.AddScore("Adam");
+employee1.AddScore("4000");
+employee1.AddScore(8.232);
+employee1.AddScore(5999); 
+//employee1.AddScore(8f);
 
-employee2.AddScore(6);
-employee2.AddScore(7);
-employee2.AddScore(3);
-employee2.AddScore(4);
-employee2.AddScore(2);
+//employee2.AddScore(6); 
+//employee2.AddScore(7);
+//employee2.AddScore(3);
+//employee2.AddScore(4);
+//employee2.AddScore(2);
 
-employee3.AddScore(5); 
-employee3.AddScore(8);
-employee3.AddScore(6);
-employee3.AddScore(5);
-employee3.AddScore(9);
+//employee3.AddScore(5); 
+//employee3.AddScore(8);
+//employee3.AddScore(6);
+//employee3.AddScore(5);
+//employee3.AddScore(9);
 
 
-//interpolacja stringow
 var statistics =employee1.GetStatistics();
 
 
-Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Average: {statistics.Average:n2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}"); 
 
 
 
-List<Employee> employees = new List<Employee>()
-{
-    employee1,  employee2,   employee3
-};
+//List<Employee> employees = new List<Employee>()
+//{
+//    employee1,  employee2,   employee3
+//};
 
-int maxResult = -1;
-Employee employeeWithMaxResult = null;
+//int maxResult = -1;
+//Employee employeeWithMaxResult = null;
 
 
-foreach (var Employee in employees) 
-{
-    if (Employee.Result > maxResult)
-    {
-        employeeWithMaxResult = Employee;
-        maxResult = Employee.Result;
-    }
-}
-Console.WriteLine("Pracownik z najwieksza iloscia punktow to:" + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname +  " " 
-+ maxResult + "punktow" );
-Console.WriteLine("Wiek" + employeeWithMaxResult.Age);
+//foreach (var Employee in employees) 
+//{
+//    if (Employee.Result > maxResult)
+//    {
+//        employeeWithMaxResult = Employee;
+//        maxResult = Employee.Result;
+//    }
+//}
+//Console.WriteLine("Pracownik z najwieksza iloscia punktow to:" + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname +  " " 
+//+ maxResult + "punktow" );
+//Console.WriteLine("Wiek" + employeeWithMaxResult.Age);
 
 
 
